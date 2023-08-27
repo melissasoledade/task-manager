3d47f2e4e6bde15089f1b8510dbc1667dbc57519
“melissasoledade” <melissa.perrone@quintoandar.com.br>
2023-08-27 14:40:16 -0300

    refactor

---
+++ db/init-scripts/database.sql
@@ -0,0 +1,31 @@
+CREATE USER teste WITH PASSWORD 'postgres';
+CREATE DATABASE "task-manager";
+GRANT ALL PRIVILEGES ON DATABASE "task-manager" TO teste;
+
+
+CREATE TABLE users (
+    user_id SERIAL PRIMARY KEY,
+    cpf VARCHAR,
+    username VARCHAR,
+    hash VARCHAR
+);
+
+CREATE TABLE tasks (
+    task_id SERIAL PRIMARY KEY,
+    name VARCHAR,
+    description TEXT,
+    priority INT,
+    status VARCHAR,
+    user_id INT,
+    FOREIGN KEY (user_id) REFERENCES users(user_id)
+);
+
+-- Exemplo em users
+INSERT INTO users (cpf, username, hash) VALUES
+    ('12345678900', 'usuario1', 'hash1'),
+    ('98765432100', 'usuario2', 'hash2');
+
+-- Inserções em tasks
+INSERT INTO tasks (name, description, priority, status, user_id) VALUES
+    ('Tarefa 1', 'Descrição da tarefa 1', 1, 'Em andamento', 1),
+    ('Tarefa 2', 'Descrição da tarefa 2', 2, 'Concluída', 2);

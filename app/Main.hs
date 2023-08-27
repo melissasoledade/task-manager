f658f6bbb8720fdabf0c5d3179a7eda5f8c3bc57
“melissasoledade” <melissa.perrone@quintoandar.com.br>
2023-08-27 14:07:42 -0300

    instal postgresql

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,19 @@
 module Main (main) where
 
+import Database.PostgreSQL.Simple
+
 import Lib
 
+localPG :: ConnectInfo
+localPG =
+  defaultConnectInfo
+    { connectHost = "0.0.0.0",
+      connectDatabase = "task-manager",
+      connectUser = "postgres",
+      connectPassword = "postgres"
+    }
+
 main :: IO ()
-main = someFunc
+main = do
+    conn <- connect localPG
+    someFunc
